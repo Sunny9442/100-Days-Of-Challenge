cb64ec418baeb1cba026c2b6858fba32d2f073e9
Sunny Shahzad <161554542+Sunny9442@users.noreply.github.com>
2024-04-20 07:27:25 -0700

    Delete Day-02 directory

--- Day-02/Q5/apps.js
+++
@@ -1,5 +0,0 @@
-//Question 3: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
-var names = "\t\n Sunny Shahzad  \t\n";
-console.log(names);
-console.log(names.trim());
-console.log(names.replace("Sunny", "Muhammad"));
