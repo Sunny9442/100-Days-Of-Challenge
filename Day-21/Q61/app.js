b8180ae119699ca73c5fad68bb6747c96bd52d30
Sunny Shahzad <161554542+Sunny9442@users.noreply.github.com>
2024-04-20 07:37:06 -0700

    Day-21

---
+++ Day-21/Q61/app.js
@@ -0,0 +1,12 @@
+// Question 61: Making Enums for Vehicles: Let's create categories for vehicles like cars, trucks, and motorcycles using enums, and show one example.
+var Vehcles;
+(function (Vehcles) {
+    Vehcles[Vehcles["Cars"] = 0] = "Cars";
+    Vehcles[Vehcles["Motorcycle"] = 1] = "Motorcycle";
+    Vehcles[Vehcles["Trucks"] = 2] = "Trucks";
+    Vehcles[Vehcles["Suzuki"] = 3] = "Suzuki";
+})(Vehcles || (Vehcles = {}));
+console.log(Vehcles.Cars);
+console.log(Vehcles.Motorcycle);
+console.log(Vehcles.Trucks);
+console.log(Vehcles.Suzuki);
---
+++ Day-21/Q61/app.ts
@@ -0,0 +1,11 @@
+// Question 61: Making Enums for Vehicles: Let's create categories for vehicles like cars, trucks, and motorcycles using enums, and show one example.
+enum Vehcles{
+    Cars,
+    Motorcycle,
+    Trucks,
+    Suzuki
+}
+console.log(Vehcles.Cars)
+console.log(Vehcles.Motorcycle)
+console.log(Vehcles.Trucks)
+console.log(Vehcles.Suzuki)
