13ee5f551413244515f537919c454ffb0e1bac61
Sunny Shahzad <161554542+Sunny9442@users.noreply.github.com>
2024-04-20 07:48:20 -0700

    Day-39

---
+++ Day-39/Q117/new.ts
@@ -0,0 +1,30 @@
+// Question 117: Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.
+function cases_match (Grades : string) : void {
+    switch (Grades){
+        case "A+" :
+            console.log("Excellent")
+            break;
+        case "A" :
+            console.log("Fabulous")
+            break;
+        case "B" :
+            console.log("Very Good")
+            break;
+        case "C" :
+            console.log("Fair")
+            break;
+        case "D" :
+            console.log("Poor")
+            break;
+        case "E" :
+            console.log("What are you doing !")
+            break;
+        case "F" :
+            console.log("Fail (Better Luck Next Time)")
+            break;
+        default :
+            console.log("Invalid Grades")
+            break;
+    }
+}
+cases_match ("A+")
