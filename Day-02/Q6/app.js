9de7b9d71beb36a0960a57ee26c74b8411e63c42
Sunny Shahzad <161554542+Sunny9442@users.noreply.github.com>
2024-04-20 07:17:04 -0700

    Day-02

---
+++ Day-02/Q6/app.js
@@ -0,0 +1,4 @@
+//Question 2: Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var favourite_person = "Bill Gates";
+var message = ("this is my favourite person & also his Personality & also His quotes & this name is".concat(favourite_person, " \u201CI choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.\u201D"));
+console.log(message);
