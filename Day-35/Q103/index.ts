05506b482bda4baf020b263cb85b7c5dc2852da3
Sunny Shahzad <161554542+Sunny9442@users.noreply.github.com>
2024-04-20 07:45:33 -0700

    Day-35

---
+++ Day-35/Q103/index.ts
@@ -0,0 +1,5 @@
+// Question 103: Write a function that returns a random boolean value, true or false.
+function Random_Boolean (): boolean {
+    return Math.random () > 0.10
+}
+console.log(Random_Boolean())
