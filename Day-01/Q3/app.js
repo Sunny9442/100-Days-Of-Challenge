063af7ca77c5303027e53e8e42bbd63899ef2677
Sunny Shahzad <161554542+Sunny9442@users.noreply.github.com>
2024-04-20 06:33:57 -0700

    Day-01

---
+++ Day-01/Q3/app.js
@@ -0,0 +1,15 @@
+//Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var namess = "Sunny shahzad";
+console.log(namess.toUpperCase());
+console.log(namess.toLowerCase());
+var app = "   Sunny   ";
+console.log(app.trim());
+console.log(namess.slice(0));
+console.log(namess.slice(1));
+console.log(namess.slice(2));
+console.log(namess.slice(3));
+console.log(namess.slice(4));
+console.log(namess.charAt(0).toUpperCase() + namess.slice(1).toLowerCase());
+console.log(namess.charAt(1).toUpperCase() + namess.slice(2).toLowerCase());
+console.log(namess.charAt(5).toUpperCase() + namess.slice(5).toLowerCase());
+console.log(namess.charAt(3));
